1f132ce0eb39a3a9cfa20af6106519a810c8e607
Zachary Lewis <zachary.lewis@thegamestudio.net>
2013-11-11 02:05:30 -0600

    Timing, task completion, alert sounds, and more.
    - Squared off corners
    - Task timing now working properly
    - Tasks can be completed
    - An alert is played when entering a block
    - Probably more stuff I forgot.

--- js/stutter.js
+++ js/stutter.js
@@ -1,20 +1,24 @@
 var source   = $("#task-template").html();
 var template = Handlebars.compile(source);
-var data = { tasks: []};
+var data = { tasks: [], completed: []};
 var currentTaskIndex;
 var currentTask;
 var timerID;
 var currentTime = 0;
-var totalTime = 0;
+var totalPhraseTime = 10;
+var totalBlockTime = 5;
 var timerRunning = false;
+var inPhrase = false;
 
-var localTasks = localStorage.getItem("tasks");
-if (localTasks != null) { data.tasks = JSON.parse(localTasks); }
+var localTasks = localStorage.getItem("stutterTasks");
+if (localTasks != null) { data = JSON.parse(localTasks); }
 
 $( "#confirmTaskButton" ).click(createTask);
 $("#taskModal").on("shown.bs.modal", function() {$("#taskName").focus();});
-$("#taskModal").on("hidden.bs.modal", clearTaskModal)
+$("#taskModal").on("hidden.bs.modal", clearTaskModal);
 $('#progressBarArea').hide();
+$('#completeButton').hide();
+$('#completeButton').click(completeTask);
 
 function createTask()
 {
@@ -22,7 +26,6 @@ function createTask()
 	var taskDescription = $("#taskDescription").val();
 	var newTask = {name: taskName, description: taskDescription};
 	data.tasks.push(newTask);
-	localStorage.setItem("tasks", JSON.stringify(data.tasks));
 	updateTasks(data);
 	$('#taskModal').modal('hide');
 	return false;
@@ -40,55 +43,88 @@ function editTask(index)
 
 }
 
-function beginTimer()
+function beginTimer(isBlock)
 {
 	window.clearInterval(timerID);
-	currentTime = 0;
-	totalTime = 5;
+
+	// If there are no valid tasks, we should stop the timer and quit.
+	if (data.tasks.length <= 0)
+	{
+		endTimer();
+		return;
+	}
 
 	$('#timerButton').removeClass('btn-primary');
 	$('#timerButton').addClass('btn-danger');
-	$('#timerButton').text("End Phrase");
+	$('#timerButton').html("<i class=\"fa fa-stop\"></i> End Phrase");
 	$("#timerButton").off('click', beginTimer);
 	$("#timerButton").on('click', endTimer);
+	$('#completeButton').show();
 
-	// Select a task to begin working on.
-	if (data.tasks.length > 0)
-	{
-		if (data.tasks.length > 1 && currentTask != null)
-		{
-			// More than one task exists. Get one that isn't the current task.
-			var newTask;
-			do
-			{
-				newTask = data.tasks[Math.floor(Math.random() * data.tasks.length)];
-			} while (newTask == currentTask);
-
-			currentTask = newTask;
-		}
-		else
-		{
-			currentTask = data.tasks[Math.floor(Math.random() * data.tasks.length)];
-		}
-
-		$('#activeTaskName').text(currentTask.name);
-		$("#activeTaskDescription").text(currentTask.description);
-	}
+	timerRunning = true;
+	currentTime = 0;
 
 	var progressBar = $('#progressBar');
-	progressBar.css("width", "0%");
 
 	// Using a closure is needed to maintain reference.
 	timerID = window.setInterval(function() {
 		currentTime++;
-		progressBar.css("width", Math.round(100 * currentTime/totalTime) + "%");
-		if (currentTime >= totalTime) { beginTimer(); }
+		if (inPhrase)
+		{
+			progressBar.css("width", Math.round(100 * currentTime/totalPhraseTime) + "%");
+			if (currentTime >= totalPhraseTime) { beginTimer(true); }
+		}
+		else
+		{
+			progressBar.css("width", 100 - Math.round(100 * currentTime/totalBlockTime) + "%");
+			if (currentTime >= totalBlockTime) { beginTimer(false); }
+		}
+		
+		
 	}, 1000);
+
+	if (isBlock === true)
+	{
+		progressBar.addClass("progress-bar-danger");
+		progressBar.css("width", "100%");
+		inPhrase = false;
+
+		document.getElementById('blockAlertAudio').play();
+	}
+	else
+	{
+		// Set up a new phrase.
+		progressBar.removeClass("progress-bar-danger");
+		progressBar.css("width", "0%");
+		inPhrase = true;
+
+		// Select a task to begin working on.
+		if (data.tasks.length > 0)
+		{
+			if (data.tasks.length > 1 && currentTask != null)
+			{
+				// More than one task exists. Get one that isn't the current task.
+				var newTask;
+				do
+				{
+					newTask = data.tasks[Math.floor(Math.random() * data.tasks.length)];
+				} while (newTask == currentTask);
+
+				currentTask = newTask;
+			}
+			else
+			{
+				currentTask = data.tasks[Math.floor(Math.random() * data.tasks.length)];
+			}
+
+			$('#activeTaskName').text(currentTask.name);
+			$('#activeTaskDescription').text(currentTask.description);
+			$('#completeButton').removeClass("disabled");
+		}
+	}
 
 	// Show the progress bar area.
 	$('#progressBarArea').slideDown(200);
-
-	timerRunning = true;
 }
 
 function endTimer()
@@ -97,23 +133,44 @@ function endTimer()
 
 	$('#timerButton').removeClass('btn-danger');
 	$('#timerButton').addClass('btn-primary');
-	$('#timerButton').text("Begin Phrase");
+	$('#timerButton').html("<i class=\"fa fa-play\"></i> Begin Phrase");
 
 	$("#timerButton").on('click', beginTimer);
 	$("#timerButton").off('click', endTimer);
 
 	// Hide the progress bar area.
 	$('#progressBarArea').slideUp(200);
+	$('#completeButton').hide();
 
+	$('#progressBar').removeClass("progress-bar-danger");
+
+	inPhrase = false;
 	timerRunning = false;
 	currentTask = null;
+}
+
+function completeTask()
+{
+	if (currentTask != null)
+	{
+		data.completed.push(currentTask);
+		taskIndex = data.tasks.indexOf(currentTask);
+		data.tasks.splice(taskIndex, 1);
+
+		// Prevent mutiple adds of completed tasks.
+		currentTask = null;
+
+		// Redraw
+		updateTasks(data);
+		$('#completeButton').addClass("disabled");
+
+		// TODO: Should we stop the timer?
+	}
 }
 
 function setCurrentTime(currentSeconds, totalSeconds)
 {
 	var percent = Math.round(currentSeconds/totalSeconds);
-	console.log(currentSeconds);
-	
 }
 
 function selectTask(index)
@@ -128,6 +185,10 @@ function selectTask(index)
 
 function updateTasks(dataObjecct)
 {
+	// Save tasks locally.
+	localStorage.setItem("stutterTasks", JSON.stringify(data));
+
+	// Render with Handlebars.
 	$("#task-placeholder").html(template(data));
 	if (data.tasks.length == 0)
 	{
